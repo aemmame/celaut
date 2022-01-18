39c2d6ae542ecdb50aa8ec2659ae9d4a312e82c1
aemmame <aemmame@disroot.org>
2022-01-18 22:49:12 +0700

    change terminology

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import argparse
 def main():
 	all_args=argparse.ArgumentParser()
 	all_args.add_argument(type=int, dest="code", default=120, help="Code for cellular automaton")
-	all_args.add_argument("-l", "--length", required=False, default=40, dest="length", type=int, help="Length of the space")
+	all_args.add_argument("-l", "--length", required=False, default=40, dest="length", type=int, help="Length of the board")
 	all_args.add_argument("-i","--interval", required=False, default=0.1, dest="interval", type=float, help="Interval between frames (in seconds)")
-	all_args.add_argument("-s","--steps", required=False, default=50, dest="steps", type=int, help="Total amount of steps")
+	all_args.add_argument("-I","--iterations", required=False, default=50, dest="steps", type=int, help="Total amount of iterations")
 	args=all_args.parse_args()
 	print(args)
 if __name__=="__main__":
