d3d9d9d52e3928ddd8e1c0c081f4fbfeb4898e1e
aemmame <aemmame@disroot.org>
2022-01-18 22:47:32 +0700

    add args

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import sys
 from time import sleep
+import argparse
 
 def main():
-	for _ in range(10):
-		print("h")
-		sleep(0.1)
-
+	all_args=argparse.ArgumentParser()
+	all_args.add_argument(type=int, dest="code", default=120, help="Code for cellular automaton")
+	all_args.add_argument("-l", "--length", required=False, default=40, dest="length", type=int, help="Length of the space")
+	all_args.add_argument("-i","--interval", required=False, default=0.1, dest="interval", type=float, help="Interval between frames (in seconds)")
+	all_args.add_argument("-s","--steps", required=False, default=50, dest="steps", type=int, help="Total amount of steps")
+	args=all_args.parse_args()
+	print(args)
 if __name__=="__main__":
 	main()
